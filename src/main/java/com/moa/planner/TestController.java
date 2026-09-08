da1afa9366dddc1a877e1913ef053da6126d543e
wonjun <afkn0337@gmail.com>
2026-09-08 23:32:02 +0900

    테스트 페이지 등록

---
+++ src/main/java/com/moa/planner/TestController.java
@@ -0,0 +1,13 @@
+package com.moa.planner;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot 정상 동작 중!";
+    }
+}
